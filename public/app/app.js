db4af67f0e6c39d10c9aac1861ff5e5b42330131
jesse10e <jesse10e@gmail.com>
2017-05-05 17:04:26 -0600

    mid commit 3

--- public/app/app.js
+++ public/app/app.js
@@ -3,15 +3,15 @@ angular.module('armoryApp', ['ui.router'])
         $stateProvider
             .state('home',{
               url:'/',
-              templateUrl:'../dist/app/views/home.html'
+              templateUrl:'./dist/app/views/home.html'
             })
             .state('pvp',{
                 url:'/pvp',
-                templateUrl: "../dist/app/views/pvp.html"
+                templateUrl: "./dist/app/views/pvp.html"
             })
             .state('characters', {
               url:'/characterlookup',
-              templateUrl:'../dist/app/views/characters.html',
+              templateUrl:'./dist/app/views/characters.html',
               controller:'charactersCtrl'
             });
 
